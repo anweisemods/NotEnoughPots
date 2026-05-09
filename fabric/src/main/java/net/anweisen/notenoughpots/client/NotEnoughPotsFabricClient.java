89bd64e99dc6ba662a1d08fe021f3f8a7934a26f
Angelo <60273047+anweisen@users.noreply.github.com>
2026-05-09 18:23:47 +0200

    feat: potted leaves

--- fabric/src/main/java/net/anweisen/notenoughpots/client/NotEnoughPotsFabricClient.java
+++ fabric/src/main/java/net/anweisen/notenoughpots/client/NotEnoughPotsFabricClient.java
@@ -27,14 +27,27 @@ public class NotEnoughPotsFabricClient implements ClientModInitializer {
     ColorProviderRegistry.BLOCK.register(mimicBlockColor(Blocks.SHORT_GRASS), NotEnoughPotsBlockType.POTTED_SHORT_GRASS.findBlock());
     ColorProviderRegistry.BLOCK.register(mimicBlockColor(Blocks.TALL_GRASS), NotEnoughPotsBlockType.POTTED_TALL_GRASS.findBlock());
     ColorProviderRegistry.BLOCK.register(mimicBlockColor(Blocks.LARGE_FERN), NotEnoughPotsBlockType.POTTED_LARGE_FERN.findBlock());
-    ColorProviderRegistry.BLOCK.register(mimicBlockColor(Blocks.BUSH), NotEnoughPotsBlockType.POTTED_BUSH.findBlock());
     ColorProviderRegistry.BLOCK.register(mimicBlockColor(Blocks.VINE), NotEnoughPotsBlockType.POTTED_VINE.findBlock());
+    ColorProviderRegistry.BLOCK.register(mimicBlockColor(Blocks.OAK_LEAVES), NotEnoughPotsBlockType.POTTED_OAK_LEAVES.findBlock());
+    ColorProviderRegistry.BLOCK.register(mimicBlockColor(Blocks.SPRUCE_LEAVES), NotEnoughPotsBlockType.POTTED_SPRUCE_LEAVES.findBlock());
+    ColorProviderRegistry.BLOCK.register(mimicBlockColor(Blocks.BIRCH_LEAVES), NotEnoughPotsBlockType.POTTED_BIRCH_LEAVES.findBlock());
+    ColorProviderRegistry.BLOCK.register(mimicBlockColor(Blocks.JUNGLE_LEAVES), NotEnoughPotsBlockType.POTTED_JUNGLE_LEAVES.findBlock());
+    ColorProviderRegistry.BLOCK.register(mimicBlockColor(Blocks.ACACIA_LEAVES), NotEnoughPotsBlockType.POTTED_ACACIA_LEAVES.findBlock());
+    ColorProviderRegistry.BLOCK.register(mimicBlockColor(Blocks.DARK_OAK_LEAVES), NotEnoughPotsBlockType.POTTED_DARK_OAK_LEAVES.findBlock());
+    // 1.17+ (AZALEA_LEAVES and FLOWERING_AZALEA_LEAVES are not tinted!)
+    // 1.19+
+    ColorProviderRegistry.BLOCK.register(mimicBlockColor(Blocks.MANGROVE_LEAVES), NotEnoughPotsBlockType.POTTED_MANGROVE_LEAVES.findBlock());
+    // 1.20+
     ColorProviderRegistry.BLOCK.register(mimicBlockColor(Blocks.PINK_PETALS), NotEnoughPotsBlockType.POTTED_PINK_PETALS.findBlock());
+    // 1.20.3+ (PALE_OAK_LEAVES are not tinted!)
+    // 1.21.5+
+    ColorProviderRegistry.BLOCK.register(mimicBlockColor(Blocks.BUSH), NotEnoughPotsBlockType.POTTED_BUSH.findBlock());
     ColorProviderRegistry.BLOCK.register(mimicBlockColor(Blocks.WILDFLOWERS), NotEnoughPotsBlockType.POTTED_WILDFLOWERS.findBlock());
 
     ColorProviderRegistry.BLOCK.register(agedStemBlockColor(Blocks.MELON_STEM, 5), NotEnoughPotsBlockType.POTTED_MELON_STEM.findBlock());
     ColorProviderRegistry.BLOCK.register(agedStemBlockColor(Blocks.PUMPKIN_STEM, 7), NotEnoughPotsBlockType.POTTED_PUMPKIN_STEM.findBlock());
 
+    // 1.13+
     ColorProviderRegistry.BLOCK.register(warmWaterBlockColor(), NotEnoughPotsBlockType.POTTED_KELP.findBlock());
     ColorProviderRegistry.BLOCK.register(warmWaterBlockColor(), NotEnoughPotsBlockType.POTTED_SEAGRASS.findBlock());
     ColorProviderRegistry.BLOCK.register(warmWaterBlockColor(), NotEnoughPotsBlockType.POTTED_TUBE_CORAL.findBlock());
