e5b62ceb9ca88e3631d9fe8aa3eb175ce2924a46
Angelo <60273047+anweisen@users.noreply.github.com>
2026-05-10 20:11:05 +0200

    feat: inital pass on 1.17 port (fabric)
    tricky: incompatible versions in multiloader template
    - gradle 8, java 16, forge 6.0+, sponge.gradle.vanilla 0.2, loom 0.10-SNAPSHOT
    ! forge not working yet

--- forge/src/main/java/net/anweisen/notenoughpots/platform/ForgePlatformBridge.java
+++ forge/src/main/java/net/anweisen/notenoughpots/platform/ForgePlatformBridge.java
@@ -3,8 +3,8 @@ package net.anweisen.notenoughpots.platform;
 import net.anweisen.notenoughpots.IPottedBlockType;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fmllegacy.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.RegistryObject;
 import java.util.EnumMap;
 import java.util.Map;
 
